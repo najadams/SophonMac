6c9e68b464dce5e785bf1cd5a1d522691f561bed
najm adams <najmadams1706@gmail.com>
2025-09-25 05:58:14 +0000

    removing the sync status for web builds

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -33,7 +33,6 @@ import {
 import { useSidebar } from "../../context/context";
 import bcrypt from "bcryptjs";
 import axios from "../../config/index";
-import SyncStatus from "../SyncStatus";
 
 export default function Header() {
   const dispatch = useDispatch();
@@ -167,11 +166,6 @@ export default function Header() {
               <h3 className="title">Sophon</h3>
             </span>
           </Typography>
-          
-          {/* Sync Status Component */}
-          <Box sx={{ flexGrow: 0, mr: 2 }}>
-            <SyncStatus companyId={user?.companyId || user?.company?.id} />
-          </Box>
           
           <div>
             <Tooltip title="Profile">
